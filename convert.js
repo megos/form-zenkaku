b532af81515585a819a411ef763bb7d1ca09d1e5
megos <gmegos.27@gmail.com>
2016-03-09 05:02:01 +0900

    外部ファイル削除

--- convert.js
+++
@@ -1,8 +0,0 @@
-self.on('click', function(node) {
-	var text = document.activeElement.value;
-	text.replace(/[A-Za-z0-9]/g, function(s) {
-    	return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
-	});
-	text.replace(/-/g, '―');
-	document.activeElement.value = text;
-});
